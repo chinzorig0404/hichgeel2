060fa5eb085b089d5d8931605591d4707d8b5db2
чинзориг <jghjhgh@nnhjh>
2020-01-26 12:23:09 +0800

    bi edit hiilee

---
+++ a.py
@@ -0,0 +1,11 @@
+# str
+owog = "otgonbayar"
+
+#len(), lower(),upper(),capitaqlize() find()...
+
+print(owog.upper())
+print("0 r temdegt:",owog[0])
+print(owog.replace("o","a"))
+print(owog.find("n"))
+
+print(owog[0:5])
